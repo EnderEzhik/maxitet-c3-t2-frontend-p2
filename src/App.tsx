6a0addb3c70e89abcb79fa52efb0bafca07519f1
EnderEzhik <Achernov2007@gmail.com>
2026-04-28 17:54:34 +0300

    Обновление стилей

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
 import Result from './pages/Result';
